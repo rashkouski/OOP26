606fab376695e6bd3ce67b0128893118ef8ef6ad
rashkouski <ryszardraszkouski9@gmail.com>
2026-02-27 11:33:52 +0100

    lab 1 zadanie 1

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
+        Point p = new Point();
+        p.x=5F;
+        p.y=7.5F;
+
+        System.out.println(p.toString());
     }
 }
---
+++ untitled/src/Point.java
@@ -0,0 +1,6 @@
+public class Point {
+    public float x,y;
+    public String toString(){
+        return "Point (x="+x+" y="+y+")";
+    }
+}
