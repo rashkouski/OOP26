eec027d3bf63a99d5008f22b75640bc561e05d74
rashkouski <ryszardraszkouski9@gmail.com>
2026-03-27 12:05:10 +0100

    lab 5 z 8

--- untitled/genealogy/src/Family.java
+++ untitled/genealogy/src/Family.java
@@ -1,16 +1,28 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Family {
-    private Map<String, Person> people= new HashMap<>();
+    private Map<String, List<Person>> people= new HashMap<>();
 
     public void add(Person... people){
         for(Person person : people){
-            this.people.put(person.name(), person);
+            String key = person.name();
+            if(this.people.containsKey(key)){
+                List<Person> temp = this.people.get(key);
+                temp.add(person);
+                temp.sort(Person::compareTo);
+            }
+            else{
+                List<Person> temp2 = new ArrayList<>();
+                temp2.add(person);
+                this.people.put(key, temp2);
+            }
         }
 
     }
-    public Person get(String key){
+    public List<Person> get(String key){
         return people.get(key);
     }
 }
--- untitled/genealogy/src/Main.java
+++ untitled/genealogy/src/Main.java
@@ -11,19 +11,19 @@ public class Main {
         people.add(new Person("Mike", "Tson", LocalDate.of(1966,5,18)));
         people.add(new Person("Snape", "Stelenski", LocalDate.of(2000,5,18)));
         people.add(new Person("Turk", "Apache", LocalDate.of(1945,5,18)));
-        System.out.println(people);
+        //System.out.println(people);
         Person parent = people.get(0);
         Person child = people.get(1);
-        System.out.println(parent.getYoungeastChild());
-        System.out.println(parent.getChildren());
+        //System.out.println(parent.getYoungeastChild());
+        //System.out.println(parent.getChildren());
 
         Family family = new Family();
         family.add(people.get(0));
         family.add(people.get(1));
         family.add(people.get(2));
+        family.add(new Person("Mike", "Tyson", LocalDate.of(1999,12,12)));
 
-        Person mike = family.get("Mike Tson");
-        System.out.println(mike);
+        System.out.println(family.get("Mike Tyson"));
 
     }
     }
